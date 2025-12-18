1305193fa819107afc788bad4ce89943ceeb8edc
radardada <gacoruncek73@gmail.com>
2025-12-19 05:29:28 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,40 @@
+// auth.js
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
+// =========================
+// LOGIN ADMIN
+// =========================
+export function adminLogin(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "admin.html";
+    })
+    .catch((error) => {
+      alert("Login gagal: " + error.message);
+    });
+}
+
+// =========================
+// PROTECT ADMIN PAGE
+// =========================
+export function protectAdminPage() {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "index.html";
+    }
+  });
+}
+
+// =========================
+// LOGOUT ADMIN
+// =========================
+export function adminLogout() {
+  auth.signOut().then(() => {
+    window.location.href = "index.html";
+  });
+}
