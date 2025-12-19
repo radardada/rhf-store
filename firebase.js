2787919583d2c04aeae60f32eb321bd7fa892ece
radardada <gacoruncek73@gmail.com>
2025-12-19 15:24:26 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAyHSaALeuwibcF22VNL_6WDlcZepcDB3A",
   authDomain: "rhf-gamestore.firebaseapp.com",
@@ -17,9 +14,6 @@ const firebaseConfig = {
   measurementId: "G-RYFR9D5R5H"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getDatabase(app);
-const analytics = getAnalytics(app);
