63f1830627401b0cb9857b05e662a44f92eb7239
radardada <gacoruncek73@gmail.com>
2025-12-20 04:54:49 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,4 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
+// firebase.js - Compat version untuk HTML biasa
 const firebaseConfig = {
   apiKey: "AIzaSyAyHSaALeuwibcF22VNL_6WDlcZepcDB3A",
   authDomain: "rhf-gamestore.firebaseapp.com",
@@ -14,6 +10,6 @@ const firebaseConfig = {
   measurementId: "G-RYFR9D5R5H"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const db = getDatabase(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
