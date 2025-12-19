b0b4a0cc1dcccb337f87461a50df1dad7899c466
radardada <gacoruncek73@gmail.com>
2025-12-19 13:55:21 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,22 @@
-// firebase.js // KONFIGURASI & INIT FIREBASE (RHF GAMES) // Dipakai oleh SEMUA file: index, game, detail, checkout, admin
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"; import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js"; import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js"; import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAyHSaALeuwibcF22VNL_6WDlcZepcDB3A",
+  authDomain: "rhf-gamestore.firebaseapp.com",
+  databaseURL: "https://rhf-gamestore-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rhf-gamestore",
+  storageBucket: "rhf-gamestore.firebasestorage.app",
+  messagingSenderId: "52065906934",
+  appId: "1:52065906934:web:c15f0eff91cc937f787cd7",
+  measurementId: "G-RYFR9D5R5H"
+};
 
-// ========================= // FIREBASE CONFIG (PUNYA KAMU) // ========================= const firebaseConfig = { apiKey: "AIzaSyAyHSaALeuwibcF22VNL_6WDlcZepcDB3A", authDomain: "rhf-gamestore.firebaseapp.com", databaseURL: "https://rhf-gamestore-default-rtdb.asia-southeast1.firebasedatabase.app", projectId: "rhf-gamestore", storageBucket: "rhf-gamestore.firebasestorage.app", messagingSenderId: "52065906934", appId: "1:52065906934:web:c15f0eff91cc937f787cd7", measurementId: "G-RYFR9D5R5H" };
-
-// ========================= // INITIALIZE // ========================= export const app = initializeApp(firebaseConfig); export const analytics = getAnalytics(app); export const auth = getAuth(app); export const db = getDatabase(app);
-
-// ========================= // GLOBAL EXPORT // ========================= export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
