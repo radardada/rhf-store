ac44ca7c0c6272ff456ce6efcf60c2c53e890532
radardada <gacoruncek73@gmail.com>
2025-12-23 07:06:15 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,12 +1,20 @@
 // utils.js
 import { db } from './firebase.js';
-import { ref, get, set, push, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { formatRupiah, getQueryParam } from './utils.js'; // Jika sudah ada
+import { ref, get, push, set, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 async function fetchProducts() {
   const productsRef = ref(db, 'products');
   const snapshot = await get(productsRef);
   return snapshot.val() || {};
+}
+
+async function addProduct(product) {
+  const newRef = push(ref(db, 'products'));
+  await set(newRef, product);
+}
+
+async function deleteProduct(id) {
+  await remove(ref(db, `products/${id}`));
 }
 
 async function fetchOrders() {
@@ -21,22 +29,15 @@ async function fetchUsers() {
   return snapshot.val() || {};
 }
 
-async function addProduct(product) {
-  const newRef = push(ref(db, 'products'));
-  await set(newRef, product);
-}
-
-async function deleteProduct(id) {
-  await remove(ref(db, `products/${id}`));
-}
-
-async function addOrder(order) {
-  const newRef = push(ref(db, 'orders'));
-  await set(newRef, order);
-}
-
 async function trackUser(user) {
-  await set(ref(db, `users/${user.uid}`), { email: user.email, lastLogin: new Date().toISOString() });
+  await set(ref(db, `users/${user.uid}`), {
+    email: user.email,
+    lastLogin: new Date().toISOString()
+  });
 }
 
-export { formatRupiah, getQueryParam, fetchProducts, fetchOrders, fetchUsers, addProduct, deleteProduct, addOrder, trackUser };
+function formatRupiah(angka) {
+  return 'Rp ' + angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export { fetchProducts, addProduct, deleteProduct, fetchOrders, fetchUsers, trackUser, formatRupiah };
